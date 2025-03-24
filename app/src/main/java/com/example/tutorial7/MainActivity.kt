fc8ddfc5de2af789e9067bd5fa80a15ccc048a9c
Pavan Uthsara <uthsarapavan@gmail.com>
2025-03-24 11:28:35 +0530

    front end v1.0 backend working

--- app/src/main/java/com/example/tutorial7/MainActivity.kt
+++ app/src/main/java/com/example/tutorial7/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.tutorial7
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    val homeFragment = HomeFragment()
+    val settingsFragment = SettingsFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val button: Button = findViewById(R.id.button)
+        val button2:Button = findViewById(R.id.button2)
+
+        button.setOnClickListener{}
+        button2.setOnClickListener{}
     }
 }
